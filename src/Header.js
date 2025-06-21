0a74bb5ee0df1e976200650be494337c58d71d64
lokesh <165912452+Glokeshvardhan@users.noreply.github.com>
2025-06-21 09:32:35 +0530

    Header.js

--- src/Header.js
+++ src/Header.js
@@ -1,34 +1,31 @@
 import React, { useState } from "react";
-import { NavLink } from "react-router-dom"; // Removed `useNavigate`
+import { NavLink } from "react-router-dom";
 import logo from "./img/logo.png";
 
 const Header = () => {
   const [isNavOpen, setIsNavOpen] = useState(false);
 
-  // Function to toggle the navbar
   const toggleNavbar = () => {
     setIsNavOpen(!isNavOpen);
   };
 
-  // Function to close the navbar when a link is clicked
-  const closeNavbar = () => {  // Fixed function name
+  const closeNavbar = () => {
     setIsNavOpen(false);
   };
 
   return (
-    <nav className="navbar navbar-expand-lg  shadow-sm p-1 fixed-top co">
-      
-      <div className="container ">
-        <NavLink className="navbar-brand" to="/Home">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark shadow-sm fixed-top">
+      <div className="container">
+        <NavLink className="navbar-brand d-flex align-items-center" to="/Home">
           <img
             src={logo}
-            className="img-fluid"
-            alt=""
-            style={{ maxHeight: "80px" }}
+            className="img-fluid me-2"
+            alt="Logo"
+            style={{ maxHeight: "50px" }}
           />
+          <span className="fw-bold">LUXURY STAY</span>
         </NavLink>
 
-        {/* Toggle Button */}
         <button
           className={`navbar-toggler ${isNavOpen ? "" : "collapsed"}`}
           type="button"
@@ -40,38 +37,33 @@ const Header = () => {
           <span className="navbar-toggler-icon"></span>
         </button>
 
-        {/* Navbar Items */}
-        <div className={`collapse navbar-collapse ${isNavOpen ? "show" : ""}`} id="collapsibleNavId">
+        <div 
+          className={`collapse navbar-collapse ${isNavOpen ? "show" : ""}`} 
+          id="collapsibleNavId"
+        >
           <ul className="navbar-nav ms-auto mt-2 mt-lg-0">
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/Home"  onClick={closeNavbar}>
-                Home
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/rooms"  onClick={closeNavbar}>
-                Rooms
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/services"  onClick={closeNavbar}>
-                Services
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/contact"  onClick={closeNavbar}>
-                Contact
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/Aboutus"  onClick={closeNavbar}>
-                About Us
-              </NavLink>
-            </li>
+            {[
+              { path: "/Home", name: "Home" },
+              { path: "/rooms", name: "Rooms" },
+              { path: "/services", name: "Services" },
+              { path: "/contact", name: "Contact" },
+              { path: "/Aboutus", name: "About Us" }
+            ].map((item) => (
+              <li className="nav-item mx-1" key={item.name}>
+                <NavLink 
+                  className={({ isActive }) => 
+                    `nav-link px-3 rounded ${isActive ? "active bg-primary" : ""}`
+                  } 
+                  to={item.path}
+                  onClick={closeNavbar}
+                >
+                  {item.name}
+                </NavLink>
+              </li>
+            ))}
           </ul>
         </div>
       </div>
-      
     </nav>
   );
 };
